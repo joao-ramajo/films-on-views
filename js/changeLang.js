ee2d4a7f2439b7da5860daef09bef0e4ca068e22
LacamJC <joaoramajo744@gmail.com>
2024-02-21 20:31:26 -0300

    Adicionando tradução restante

--- js/changeLang.js
+++ js/changeLang.js
@@ -7,17 +7,19 @@ const elements=[
      document.getElementById('titulo'),
       document.getElementById('paragrafo'),
        document.getElementById('a1'),
-        document.getElementById('a2')
+        document.getElementById('a2'),
+         document.getElementById('trailer')
     ]
 
 
 
 const dataPT = {
-    autor : '<a href="https://pt.wikipedia.org/wiki/Hayao_Miyazaki">HAYAO MIYAZAKI</a>',
+    autor : '<a href="https://pt.wikipedia.org/wiki/Hayao_Miyazaki" target="_BLANK">HAYAO MIYAZAKI</a>',
     titulo : 'A VIAGEM DE CHIHIRO',
     paragrafo : 'Chihiro chega a um mundo mágico dominado por uma bruxa. Aqueles que a desobedecem são transformados em animais.',
     btn1 : 'Assistir agora',
-    btn2 : 'Assista o trailer'
+    btn2 : 'Assista o trailer',
+    trailer : 'Trailer'
 }
 
 ptBR.addEventListener('click', ()=>{
@@ -30,14 +32,16 @@ ptBR.addEventListener('click', ()=>{
     elements[2].innerHTML = dataPT.paragrafo;
     elements[3].innerHTML = dataPT.btn1;
     elements[4].innerHTML = dataPT.btn2;
+    elements[5].innerHTML = dataPT.trailer;
 })
 
 const dataEN ={
-    autor: '<a href="https://en.wikipedia.org/wiki/Hayao_Miyazaki">HAYAO MIYAZAKI</a>',
-    titulo: 'SPIRITED AWAY',
-    paragrafo: 'Chihiro arrives in a magical world ruled by a witch. Those who disobey are turned into animals.',
-    btn1: 'Watch Now',
-    btn2: 'Watch the Trailer'
+    autor: '<a href="https://en.wikipedia.org/wiki/Hayao_Miyazaki" target="_blank">Hayao Miyazaki</a>',
+    titulo: 'A Viagem de Chihiro',
+    paragrafo: 'Chihiro chega a um mundo mágico dominado por uma bruxa. Aqueles que a desobedecem são transformados em animais.',
+    btn1: 'Assistir agora',
+    btn2: 'Assista o trailer',
+    trailer: 'Trailer'
 }
 
 enUS.addEventListener('click', ()=>{
@@ -50,14 +54,17 @@ enUS.addEventListener('click', ()=>{
     elements[2].innerHTML = dataEN.paragrafo;
     elements[3].innerHTML = dataEN.btn1;
     elements[4].innerHTML = dataEN.btn2;
+    elements[5].innerHTML = dataEN.trailer;
 })
 
 const dataJP = {
-    autor: '<a href="https://en.wikipedia.org/wiki/Hayao_Miyazaki">宮崎駿</a>',
+    autor: '<a href="https://ja.wikipedia.org/wiki/%E5%AE%AE%E5%B4%8E%E9%A7%BF" target="_blank">宮崎駿</a>',
     titulo: '千と千尋の神隠し',
     paragrafo: '千尋は魔法の世界に到着し、魔女に支配されています。服従しない者は動物に変えられます。',
     btn1: '今すぐ視聴',
-    btn2: '予告編を見る'
+    btn2: '予告編を見る',
+    trailer: 'トレーラー'
+    
 }
 
 jaJP.addEventListener('click', ()=>{
@@ -70,4 +77,5 @@ jaJP.addEventListener('click', ()=>{
     elements[2].innerHTML = dataJP.paragrafo;
     elements[3].innerHTML = dataJP.btn1;
     elements[4].innerHTML = dataJP.btn2;
+    elements[5].innerHTML = dataJP.trailer;
 })
