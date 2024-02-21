8873004f978c98e33369aa04fa00c8019b72c4cf
LacamJC <joaoramajo744@gmail.com>
2024-02-21 15:41:43 -0300

    Esqueci oq eu fiz

---
+++ js/links.js
@@ -0,0 +1,11 @@
+const go_to_film = document.getElementById('a1')
+const go_to_trailer = document.getElementById('a2')
+go_to_film.addEventListener('click', ()=>{
+    var url = 'https://www.netflix.com/br/title/60023642';
+    window.open(url, '_blank');
+})
+
+go_to_trailer.addEventListener('click', ()=>{
+    var url = 'https://www.youtube.com/watch?v=70NhEBzLKU8';
+    window.open(url, '_blank');
+})
